a4d9e31e0268b77887ba59fb29a36594bc3e7991
Vachan V Y <vachanvy05@gmail.com>
2024-12-15 14:57:38 +0530

    dyn: dynamic dispatch

--- pkgx5/src/main.rs
+++ pkgx5/src/main.rs
@@ -1,3 +1,50 @@
+trait Speak {
+    fn say_hello(&self);
+}
+
+struct Dog;
+struct Cat;
+
+impl Speak for Dog {
+    fn say_hello(&self) {
+        println!("\tWoof!");
+    }
+}
+
+impl Speak for Cat {
+    fn say_hello(&self) {
+        println!("\tMeow!");
+    }
+}
+
+fn greet(speaker: &dyn Speak) {
+    speaker.say_hello();
+}
+
+// Without dyn (Compile-Time Dispatch)
+fn greet_static<T: Speak>(speaker: &T) {
+    speaker.say_hello();
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let dog = Dog;
+    let cat = Cat;
+
+    print!("Dynamic Dispatch\n");
+    greet(&dog as &dyn Speak);
+    greet(&cat as &dyn Speak);
+    print!("Static Dispatch\n");
+    greet_static(&dog);
+    greet_static(&cat);
+
+    let animals: Vec<Box<dyn Speak>> = vec![
+        Box::new(Dog),
+        Box::new(Cat),
+    ];
+    print!("Dynamic Dispatch with Vec\n");
+    for animal in animals {
+        animal.say_hello(); // Dynamically dispatched
+    }
+
 }
