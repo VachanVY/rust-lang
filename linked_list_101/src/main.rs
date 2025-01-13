ad85113dbd4699b29bb297b957dab730b1688906
Vachan V Y <vachanvy05@gmail.com>
2025-01-13 15:57:50 +0530

    linked list, done!

--- linked_list_101/src/main.rs
+++ linked_list_101/src/main.rs
@@ -1,15 +1,15 @@
 #![allow(warnings)]
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct List {
-    el: i32,
+    el: Option<i32>,
     next: Box<Option<List>>
 }
 
 impl List {
     fn new(val: i32) -> List {
         return List {
-            el: val,
+            el: Some(val),
             next: Box::new(None)
         }
     }
@@ -27,6 +27,10 @@ impl List {
     }
 
     fn append(&mut self, val: i32) {
+        if self.el.is_none() {
+            self.el = Some(val);
+            return;
+        }
         let mut current = self;
         while let Some(ref mut next_node) = *current.next {
             current = next_node;
@@ -35,27 +39,55 @@ impl List {
     }
 
     fn pop(&mut self) -> Option<i32> {
-        let current = &mut self.clone();
-        loop {
-            if current.next.is_none() {
-                let val = current.el;
-                *current = current.next.take().unwrap();
-                return Some(val)
-            }
-            // current = (*(current.next))
-            if let Some(next_node) = &mut (*current.next) {
-                // ERROR WHEN CURRENT = NEXT_NODE
+        match &mut*self.next {
+            None => {
+                let pop_val = self.el;
+                self.el = None;
+                return pop_val;
+            },
+
+            // we check the next element
+            Some(n) => {
+                if n.next.is_none() {
+                    let pop_val = n.el;
+                    // if the next element is the terminal element
+                    // we take the self.next
+                    // that will change the current.next to None
+                    self.next.take();
+                    return pop_val;
+                }else{
+                    // if the next element is not the terminal element
+                    return n.pop()
+                }
             }
         }
     }
 
-
     fn prepend(&mut self, val:i32) {
+        if self.el.is_none() {
+            self.el = Some(val);
+            return;
+        }
         // std::mem::replace(dest, src): 
         //    Moves src into the referenced dest, returning the previous dest value
         let old_self = std::mem::replace(self, List::new(val));
         self.next = Box::new(Some(old_self));
     }
+
+    fn popf(&mut self) -> Option<i32> {
+        let pop_val = self.el; // Store the value to return
+        if let Some(next_node) = self.next.take() {
+            // Update the current node with the values of the next node
+            self.el = next_node.el;
+            self.next = next_node.next;
+        } else {
+            // If there's no next node, this is the last node
+            self.el = None;
+        }
+        return pop_val;
+    }
+    
+    
 }
 
 impl std::fmt::Display for List {
@@ -64,43 +96,60 @@ impl std::fmt::Display for List {
 
         let mut current = Some(self);
         while let Some(node) = current {
-            write!(f, "{}", node.el)?;
-            current = match &*node.next {
-                Some(next_node) => Some(next_node), // Move to the next node
-                None => None,                       // End of the list
-            };
-            if current.is_some() {
-                write!(f, ", ")?;
+            if !node.el.is_none(){
+                write!(f, "{}", node.el.unwrap())?;
             }
+                current = match &*node.next {
+                    Some(next_node) => Some(next_node), // Move to the next node
+                    None => None,                       // End of the list
+                };
+                if current.is_some() {
+                    write!(f, ", ")?;
+                }
 
         }
-        writeln!(f, "])")
+        write!(f, "])")
     }
 }
 
 fn main() {
-    let mut list = List::new(42);
+    let mut list = List::new(42); 
+    println!("{}", list); // [42]
+    println!("POP: {:?}", list.pop()); // 42
+    println!("{}", list); // []
+    println!("{:?}", list.pop()); // poping from empty linked list // does nothing
+    println!("{}", list); // []
+    list.prepend(420);
+    println!("{}", list); // [420]
+    println!("POP: {:?}", list.pop()); // 420
+    println!("{}", list); // []
+    println!("POPF: {:?}", list.popf());
+    println!("{}", list); // []
+    
+
     list.append(32);
     list.append(2321);
     list.append(2839);
     list.prepend(69);
-    println!("{}", list);
+    println!("{}", list); // [69, 32, 2321, 2839]
+    println!("POPF: {:?}", list.popf()); // 69
+    println!("{}", list); // [32, 2321, 2839]
+    println!("POP: {:?}", list.pop()); // 2839
+    println!("{}", list); // [32, 2321]
 
     let mut list = List::from(&[1, 2, 3]);
+    println!("{}", list);
     list.prepend(0);
     println!("{}", list);
+    while let Some(val) = list.pop() {
+        println!("POP: {}", val);
+    }
+
+    let mut list = List::from(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    println!("{}", list);
+    list.prepend(-1);
+    println!("{}", list);
+    while let Some(val) = list.popf() {
+        println!("POPF: {}", val);
+    }
 }
-
-// unsafe {
-//     let mut current = self;
-
-//     while let Some(ref next_node) = *current.next {
-//         if next_node.next.is_none() { // | ... | current | next_node | None |
-//             let pop_val = next_node.el;
-//             current.next = Box::new(None);
-//             return Some(pop_val)
-//         }
-//         current = &mut next_node;
-//     }
-// }
-// return None
